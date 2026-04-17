4dd79832d55c1cde404eb306d16292679da4d316
ovijet halder <ovijethalder143@gmail.com>
2026-04-17 16:47:41 +0600

    vercel

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './Component/homePage/HomePage';
 import AllBooks from './Component/homePage/AllBooks';
 import AllBooksDetails from './Component/homePage/AllBooksDetails';
+import Footer from './Component/Footer';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <HomePage></HomePage>
             <AllBooks></AllBooks>
             {/* <AllBooksDetails></AllBooksDetails> */}
+            <Footer></Footer>
         </div>
     );
 };
